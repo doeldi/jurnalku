dc9af949c954fac72cd067d08aeeba7561bdd3c9
ZahranFairuz <zahranfairuzrahman@smkwikrama.sch.id>
2025-12-12 08:20:16 +0700

    update2

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jurnalku/jurnal_page.dart';
 import 'package:jurnalku/login.dart';
+import 'package:jurnalku/explore_login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Login(),
+      home: const ExploreLogin(),
     );
   }
 }
